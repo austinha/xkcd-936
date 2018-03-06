160df02044ce52e50999ea277dab48992ac868d7
Austin Ha <austinha@Austins-MacBook-Pro.local>
2018-03-05 19:47:39 -0800

    First working implementation of pgen

---
+++ src/PasswordGenerator.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class PasswordGenerator {
+	// Path to dictionary of common english words
+	public static final String DICTIONARY_PATH = "google-10000-english/google-10000-english-usa-no-swears-medium.txt";
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		int passLength;
+		if (args.length != 1) {
+			System.out.println("<Defaulting to length 4 password>");
+			passLength = 4;
+		} else {
+			passLength = Integer.parseInt(args[0]);
+		}
+		
+		ArrayList<String> words = readWords(new File(DICTIONARY_PATH));
+		String passElements = buildPass(words, passLength);
+		String pass =  passElements.replace(" ", "");
+		
+		System.out.println("xkcd-ified password (length " + pass.length() + "):");
+		System.out.println("   " + passElements);
+		System.out.println();
+		System.out.println("   " + pass);
+	}
+	
+	public static String buildPass(ArrayList<String> words, int passLength) {
+		String pass = "";
+		
+		Random r = new Random();
+		
+		for (int i = 0; i < passLength; i++) {
+			pass += words.get(r.nextInt(words.size())) + " ";
+		}
+		
+		return pass;
+	}
+	
+	public static ArrayList<String> readWords(File f) throws FileNotFoundException {
+		Scanner input = new Scanner(f);
+		
+		ArrayList<String> words = new ArrayList<String>();
+
+		while (input.hasNextLine()) {
+			words.add(input.nextLine());
+		}
+		
+		return words;
+	}
+
+}
